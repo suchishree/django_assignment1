9a9aa7525c09fa2ac446208a4192c106bcdd6bf4
suchishree <suchishree99@gmail.com>
2021-06-21 17:01:06 +0530

    2nd push

---
+++ Project12/app12/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from app12.models import Registration
+
+def showhomepage(request):
+    return render(request, 'main.html')
+
+
+def showregistrationpage(request):
+    if request.method == "GET":
+        return render(request, "register.html")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        contact = request.POST.get('contact_no')
+        location = request.POST.get('location')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        Registration(name=name, contact=contact, location=location, email=email, password=password).save()
+
+        return render(request, "register.html", {"message": "registration is done"})
+
+
+
+def showloginpage(request):
+    if request.method == "GET":
+        return render(request, "login.html")
+    else:
+        em = request.POST.get("email")
+        pw = request.POST.get("password")
+        try:
+            stu_obj = Registration.objects.get(email=em, password=pw)
+            return render(request, "welcome.html")
+        except Registration.DoesNotExist:
+            return render(request, "login.html", {"error_message": "invalid user"})
+
+
